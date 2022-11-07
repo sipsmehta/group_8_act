9aa73a98e45793ba8e8bbc87f1c3e2e73f5dc00e
PrinceKumar79994 <83058153+PrinceKumar79994@users.noreply.github.com>
2022-11-08 00:24:50 +0530

    updates in aircraft, runway and flightManager class

---
+++ src/air/traffic/controller/aircraft.java
@@ -0,0 +1,31 @@
+public class aircraft {
+    boolean emergency;
+    String planeType;
+    int currFuel;
+    double altitude;
+    int speed;
+    aircraft(boolean emergency,String planeType, int currFuel, double altitude,int speed)
+    {
+        this.emergency = emergency;
+        this.planeType = planeType;
+        this.currFuel = currFuel;
+        this.altitude =  altitude;
+        this.speed = speed;
+    }
+    void ascend()
+    {
+    
+    }
+    void descend()
+    {
+
+    }
+    void approachToLandingRunway(runway allocatedRunway)
+    {
+
+    }
+    void abortLanding(runway allocRunway)
+    {
+
+    }
+}
---
+++ src/air/traffic/controller/runway.java
@@ -0,0 +1,25 @@
+public class runway 
+{
+    boolean occupied;
+    boolean emergencyRunway;
+    runway(boolean emergencyRunway)
+    {
+        this.emergencyRunway = emergencyRunway;
+    }
+    void allocateRegularRunway(aircraft occupyingAircraft )
+    {
+        occupied = true;
+    }
+    void deallocateRegularRunway(aircraft occupyingAircraft)
+    {
+        occupied = false;
+    }
+    void allocateEmergencyRunway(aircraft occupyingAircraft)
+    {
+
+    }
+    void deallocateEmergencyRunway()
+    {
+        
+    }
+}
